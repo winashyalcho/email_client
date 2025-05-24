ddc6c8e98b0e41fa3b5030dc4ee1a940cbfc985a
winashyalcho <ashwinchoyal2050@gmail.com>
2025-05-25 01:56:21 +0530

    first build issues for react version compatibility

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,5 +1,5 @@
 import { db } from "@/server/db";
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"; 
 
 export async function POST(request: Request) {
   try {
